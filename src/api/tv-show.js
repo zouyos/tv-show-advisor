61aefb3fd4094fc6453820ce8117bb41c961cc04
zouyos <hamza.benketaf@gmail.com>
2023-12-19 15:07:41 +0100

    .env and readme.md

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,22 +1,24 @@
 import axios from "axios";
-import { BASE_URL, API_KEY_PARAM } from "../config";
+import { BASE_URL } from "../config";
 
 export class TVShowAPI {
   static async fetchPopular() {
-    const response = await axios.get(`${BASE_URL}/tv/popular${API_KEY_PARAM}`);
+    const response = await axios.get(
+      `${BASE_URL}/tv/popular?api_key=${process.env.REACT_APP_API_KEY}`
+    );
     return response.data.results;
   }
 
   static async fetchRecommendations(tvShowId) {
     const response = await axios.get(
-      `${BASE_URL}/tv/${tvShowId}/recommendations${API_KEY_PARAM}`
+      `${BASE_URL}/tv/${tvShowId}/recommendations?api_key=${process.env.REACT_APP_API_KEY}`
     );
     return response.data.results;
   }
 
   static async fetchByTitle(title) {
     const response = await axios.get(
-      `${BASE_URL}/search/tv${API_KEY_PARAM}&query=${title}`
+      `${BASE_URL}/search/tv?api_key=${process.env.REACT_APP_API_KEY}&query=${title}`
     );
     return response.data.results;
   }
